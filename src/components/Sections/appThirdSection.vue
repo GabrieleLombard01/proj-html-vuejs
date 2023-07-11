d3c8083432e8c9a287fbd61dc38b819f052d501d
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-11 12:56:59 +0200

    third sect script add

---
+++ src/components/Sections/appThirdSection.vue
@@ -0,0 +1,13 @@
+<script>
+import appTitle from '../appTitle.vue';
+import { data } from '../../data/index.js';
+
+export default {
+    components: { appTitle },
+    computed: {
+        titleThirdSection() {
+            return data.titleThirdSection[0];
+        },
+    },
+};
+</script>
