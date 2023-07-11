e322b566e56257374b0b5d99094d392bc3ef6301
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-11 12:00:30 +0200

    appTitle add

---
+++ src/components/Sections/appFirstSection.vue
@@ -0,0 +1,25 @@
+<script>
+import appTitle from '../appTitle.vue'
+export default {
+    components: { appTitle },
+    computed: {
+
+    },
+};
+</script>
+
+<template>
+    <section>
+        <div class="margin_contents">
+            <appTitle />
+        </div>
+    </section>
+</template>
+
+<style scoped>
+section {
+    background-color: white;
+    width: 100%;
+    height: 80vh;
+}
+</style>
